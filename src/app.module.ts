4da81e290fc3f47e57327d97fcf69661f3a51c31
Denise Case <denisecase@gmail.com>
2021-01-09 19:00:37 -0600

    Add quest resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { QuestModule } from './quest/quest.module';
+
 
 @Module({
-  imports: [],
   controllers: [AppController],
   providers: [AppService],
+  imports: [QuestModule],
 })
 export class AppModule {}
