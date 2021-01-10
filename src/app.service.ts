c831dfdfebc9580e9c35e58cad81b6c46b2757a8
Denise Case <denisecase@gmail.com>
2021-01-09 18:52:49 -0600

    Add endpoint /welcome

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,9 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello world - from Dr. Case!';
+  }
+  getWelcome(): string {
+    return 'Welcome to the world, babies!';
   }
 }
